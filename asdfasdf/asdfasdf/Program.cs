6f2104376e0d7437099c96d9d9961cf96e04e9f4
SeedPaths-User <lessdismissive@email.com>
2014-08-28 00:17:42 -0600

    week1practicewhaaat
    Lost as for what to do up to my code...  Additional resources needed
    with examples.

---
+++ asdfasdf/asdfasdf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asdfasdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            {
+                Console.WriteLine(number * 2);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ testtest/testtest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testtest
+{
+    class Program
+    {
+        static void Main(string[] args);
+
+
+        class name
+        {
+            static void Greeting(string name = "alex")
+            {
+                Console.WriteLine("Hello " + name);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+                 
+            
+        
+
